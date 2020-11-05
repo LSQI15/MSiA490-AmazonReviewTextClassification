a9101e7b4efb262e9ceb8e0df889fb42e09cbaf3
LSQI15 <siqi.li15@gmail.com>
2020-11-04 20:06:02 -0600

    added a svm model with unigrams, bigrams, and trigrams

--- svm.py
+++ svm.py
@@ -12,25 +12,30 @@ def train_test_svm_models(X_train, X_test, y_train, y_test):
     :param y_train: y (labels) in the training set
     :param y_test: y (labels) in the test set
     """
-    # baseline linear SVM model
-    print('\nlinearSVM_model0')
-    linearSVM_model0 = LinearSVC(random_state=115)
-    model_training_testing(linearSVM_model0, X_train, X_test, y_train, y_test, (1, 1), 'Model_Results/SVM_0')
+    # # baseline linear SVM model
+    # print('\nlinearSVM_model0')
+    # linearSVM_model0 = LinearSVC(random_state=115)
+    # model_training_testing(linearSVM_model0, X_train, X_test, y_train, y_test, (1, 1), 'Model_Results/SVM_0')
+    #
+    # # linear SVM model 1
+    # print('\nlinearSVM_model1')
+    # linearSVM_model1 = LinearSVC(random_state=115)
+    # model_training_testing(linearSVM_model1, X_train, X_test, y_train, y_test, (1, 2), 'Model_Results/SVM_1')
+    #
+    # # linear SVM model 2
+    # print('\nlinearSVM_model2')
+    # linearSVM_model2 = LinearSVC(random_state=115, dual=False)
+    # model_training_testing(linearSVM_model2, X_train, X_test, y_train, y_test, (1, 1), 'Model_Results/SVM_2')
+    #
+    # # linear SVM model 3
+    # print('\nlinearSVM_model3')
+    # linearSVM_model3 = LinearSVC(random_state=115, loss='hinge')
+    # model_training_testing(linearSVM_model3, X_train, X_test, y_train, y_test, (1, 1), 'Model_Results/SVM_3')
 
-    # linear SVM model 1
-    print('\nlinearSVM_model1')
+    # linear SVM model 4
+    print('\nlinearSVM_model4')
     linearSVM_model1 = LinearSVC(random_state=115)
-    model_training_testing(linearSVM_model1, X_train, X_test, y_train, y_test, (1, 2), 'Model_Results/SVM_1')
-
-    # linear SVM model 2
-    print('\nlinearSVM_model2')
-    linearSVM_model2 = LinearSVC(random_state=115, dual=False)
-    model_training_testing(linearSVM_model2, X_train, X_test, y_train, y_test, (1, 1), 'Model_Results/SVM_2')
-
-    # linear SVM model 3
-    print('\nlinearSVM_model3')
-    linearSVM_model3 = LinearSVC(random_state=115, loss='hinge')
-    model_training_testing(linearSVM_model3, X_train, X_test, y_train, y_test, (1, 1), 'Model_Results/SVM_3')
+    model_training_testing(linearSVM_model1, X_train, X_test, y_train, y_test, (1, 3), 'Model_Results/SVM_4')
 
 
 def main():
